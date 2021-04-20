79fe78d811bc8e82ccc2c5fa83e673d5d0a4c8f0
Kellen <kschneidewen@wisc.edu>
2021-04-20 18:00:59 -0500

    Can search by dish now

--- search.php
+++ search.php
@@ -17,7 +17,7 @@
     }
 ?>
 <body>
-    <form action="search.php?" method="post">  
+    <form action="search.php" method="post">  
             <p>
                 <label for="rest_name">Search by Restaurant: </label>
                 <select id="rest_name" name="rest_name">
@@ -33,10 +33,10 @@
                         }
                     ?>
                 </select>
-                <input type="submit" name="submit1" value="Search">
+                <input type="submit" name="submit_restaurant" value="Search">
             </p> 
     </form>
-    <form>
+    <form action="search.php" method="post">
             <p>
                 <label for="dish_name">Search by Dish: </label>
                 <select id="dish_name" name="dish_name">
@@ -52,11 +52,12 @@
                         }
                     ?>
                 </select>
-                <input type="submit" name="submit2" value="Search">
+                <input type="submit" name="submit_dish" value="Search">
             </p>
     </form>
+
     <?php
-        function restaurant()
+        function restaurant() //Code to display reviews based on restaurant
         {
             $myUserName = "id16374744_foodies";
             $myPassword = "IS424Group2!";
@@ -104,10 +105,64 @@
                 echo "no reviews posted yet";
             }
         }
-        if(isset($_POST['submit1']))
+        if(isset($_POST['submit_restaurant']))
         {
             restaurant();
         }
+
+        function dish() //code to display reviews based on dish
+        {
+            $myUserName = "id16374744_foodies";
+            $myPassword = "IS424Group2!";
+            $myLocalHost = "localhost";
+            $myDB = "id16374744_foodiesdb";
+    
+            $conn = mysqli_connect($myLocalHost, $myUserName, $myPassword, $myDB);
+            if (!$conn) {
+                die ("Connection failed: ");
+            }
+            $dish_name = $_POST['dish_name'];
+            $dish_ID_grab = "SELECT * FROM `Dish` WHERE `dish_name` = '$dish_name'";
+            $dish_ID_result = mysqli_query($conn, $dish_ID_grab);
+            $dish_ID_row = mysqli_fetch_array($dish_ID_result);
+            $dish_ID = $dish_ID_row['dish_ID'];
+
+            $review_list_sql = "SELECT * FROM `Review` WHERE `dish_ID` = '$dish_ID'";
+            $dish_based_result = mysqli_query($conn, $review_list_sql); // Run the query and test if successful
+	        if (!$dish_based_result) {
+					die("cannot proceed select query");
+	        }
+
+            echo "<h1>Reviews for ".$dish_name."</h1>";
+
+            $num = mysqli_num_rows($dish_based_result);
+			if ($num > 0){
+                echo "<table border=`1`>";
+                echo "<tr height=40><td width=100>Date</td><td width=400>Restaurant</td><td width=50>Score</td><td width=500>Review</td></tr>";
+				while($review_row = mysqli_fetch_assoc($dish_based_result)) {
+                    $rest_ID = $review_row["rest_ID"];
+                    $find_rest_sql = "SELECT * FROM `Restaurant` WHERE `rest_ID` = $rest_ID";
+                    $rest_name_result = mysqli_query($conn, $find_rest_sql);
+                    $rest_row = mysqli_fetch_array($rest_name_result);
+                    $rest_name = $rest_row['rest_name'];
+
+					echo "<tr height=40>";
+                    echo "<td width=100>". $review_row["time"] . "</td>";
+                    echo "<td width=400>" . $rest_name . "</td>";
+                    echo "<td width=50>" . $review_row["score"] . "</td>";
+					echo "<td width=500>" . $review_row["content"] . "</td>";
+					echo "</tr>";
+				}
+                echo "</table><br><br><br><br>";
+            } else {
+                echo "no reviews posted yet";
+            }
+        }
+        if(isset($_POST['submit_dish']))
+        {
+            dish();
+        }
+
     ?>
 </body>
 </html>
