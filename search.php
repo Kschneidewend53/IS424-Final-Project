48ed736815bb8412521c71426cec498a37e7df6f
Kellen <kschneidewen@wisc.edu>
2021-04-20 18:09:52 -0500

    Updated CSS on search.php

--- search.php
+++ search.php
@@ -4,6 +4,50 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+
+<!-- site metas -->
+
+<meta name="keywords" content="">
+
+<meta name="description" content="">
+
+<meta name="author" content="">	
+
+<!-- bootstrap css -->
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+<!-- style css -->
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<!-- Responsive-->
+
+<link rel="stylesheet" href="css/responsive.css">
+
+<!-- fevicon -->
+
+<link rel="icon" href="images/fevicon.png" type="image/gif"/>
+
+<!-- Scrollbar Custom CSS -->
+
+<link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+
+<!-- Tweaks for older IEs-->
+
+<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+
+<!-- owl stylesheets --> 
+
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+
 </head>
 <?php
     $myUserName = "id16374744_foodies";
@@ -17,6 +61,96 @@
     }
 ?>
 <body>
+
+    <div class="header_section">
+
+    <div class="container-fluid">
+
+<div class="row">
+
+    <div class="col-sm-12 col-lg-3">
+
+        <div class="logo"><img src="images/mflogo.jpg" height=5></div>
+
+    </div>
+
+    <div class="col-sm-4 col-lg-5">
+
+        <div class="menu-area">
+
+        <nav class="navbar navbar-expand-lg ">
+
+            <!-- <a class="navbar-brand" href="#">Menu</a> -->
+
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+            <i class="fa fa-bars"></i>
+
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                <ul class="navbar-nav mr-auto">
+
+                   <li class="nav-item active">
+
+                    <a class="nav-link active" href="index.html">Home<span class="sr-only">(current)</span></a> </li>
+
+                   <li class="nav-item">
+
+                    <a class="nav-link" href="https://madisonfoodies.000webhostapp.com/sign_up.php">Sign Up</a></li>
+
+                   <li class="nav-item">
+
+                    <a class="nav-link" href="https://madisonfoodies.000webhostapp.com/Sign-in.php">Sign In</a></li>
+
+                   <li class="#" href="#">
+
+                      <a class="nav-link" href="FAQ.html">FAQs</a></li>
+
+                   <li class="nav-item" href="#">
+
+                      <a class="nav-link" href="terms.html">Terms & Conditions</a></li>
+
+                    <li class="nav-item">
+
+                    <a class="nav-link" href="https://madisonfoodies.000webhostapp.com/search.php">Search</a></li>
+
+
+                </ul>
+
+            </div>
+
+        </nav>
+
+    </div>
+
+    </div>
+
+    <div class="col-sm-8 col-lg-4">
+
+        <div class="togle_3">
+
+      <div class="left_main">
+
+         <div class="menu_main">
+
+            <a href="https://madisonfoodies.000webhostapp.com/Sign-in.php"><i class="fa fa-fw fa-user"></i> Login</a>
+
+         </div>
+
+      </div>
+
+     </div>
+
+    </div>
+
+</div>
+
+</div>
+
+    </div>
+    <h1>Search Options</h1>
     <form action="search.php" method="post">  
             <p>
                 <label for="rest_name">Search by Restaurant: </label>
